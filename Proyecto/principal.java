b56a18058c46db8f7efe7e6c54d710f90b3c9dc1
PaulQuezada <83780330+PaulQuezada@users.noreply.github.com>
2021-05-06 16:04:13 -0500

    Update principal.java

--- Proyecto/principal.java
+++ Proyecto/principal.java
@@ -3,6 +3,6 @@ package Proyecto;
 public class principal {
     public static void main(String[] args){
         System.out.println("Hola xdxd");
-        System.out.println("CHERNOBYL");
+        System.out.println("QUE LLICO HELADITO");
     }
 }
