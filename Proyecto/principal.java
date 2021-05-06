7a6c0456d68bfaef38903589fde1b7645036045c
PaulQuezada <pquezada19@alumnos.utalca.cl>
2021-05-06 18:00:52 -0500

    PRUEBAS
    son pruebas de como funciona git

--- Proyecto/principal.java
+++ Proyecto/principal.java
@@ -4,5 +4,8 @@ public class principal {
     public static void main(String[] args){
         System.out.println("Hola xdxd");
         System.out.println("QUE LLICO HELADITO");
+        System.out.println("QUE LLICO HELADITO");
+        System.out.println("QUE LLICO HELADITO");
+        System.out.println("QUE LLICO HELADITO");
     }
 }
